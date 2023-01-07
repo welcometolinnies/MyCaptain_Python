3bbaa78a2916afffd849d76a9b5c0ecc08af617e
Alina <122103499+welcometolinnies@users.noreply.github.com>
2023-01-07 23:38:12 +0400

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -3,7 +3,7 @@ ext=fn.split(".")
 D={"py":"python","java":"java","docx":"Microsoft Word file","pdf":"PDF file","txt":"Plain text file","cpp":"C++ source code file","jpeg":"JPEG image","png":"PNG image"}
 ext1=ext[-1]
 if ext1 in D:
-    Val=D.get(ext1)
-    print("The file extension is :",Val)
+    DictVal=D.get(ext1)
+    print("The file extension is :",DictVal)
 else:
-    print("Unrecognizable filename")
+    print("Unrecognizable file extension")
