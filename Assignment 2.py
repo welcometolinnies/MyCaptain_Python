c0ce2abebab9966dfec856c9fe54f0760960f8f9
Alina <122103499+welcometolinnies@users.noreply.github.com>
2023-01-07 23:34:25 +0400

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,9 @@
+fn=input("Input the Filename: ")
+ext=fn.split(".")
+D={"py":"python","java":"java","docx":"Microsoft Word file","pdf":"PDF file","txt":"Plain text file","cpp":"C++ source code file","jpeg":"JPEG image","png":"PNG image"}
+ext1=ext[-1]
+if ext1 in D:
+    Val=D.get(ext1)
+    print("The file extension is :",Val)
+else:
+    print("Unrecognizable filename")
